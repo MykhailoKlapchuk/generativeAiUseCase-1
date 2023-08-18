607d37f6dbf61b26113311b8f9dc64a36158db9e
Mykhailo Klapchuk <mklap@softserveinc.com>
2023-08-18 12:09:37 +0200

    UC#1 Create endpoint

---
+++ UC#1/Controllers/CountryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Formats.Asn1;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UC_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        // GET: api/<CountryController>
+        [HttpGet]
+        public async Task<string> GetCountries(
+            string nameFilter = "not",
+            int populationFilter = 0,
+            string nameSort = "not",
+            int numberOfPages = 0)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = "https://restcountries.com/v3.1/all";
+                var response = await client.GetAsync(url);
+                var countryData = await response.Content.ReadAsStringAsync();
+                var list = JsonSerializer.Deserialize<List<object>>(countryData);
+                return countryData;
+            }
+        }
+    }
+}
