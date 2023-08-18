1c02e4240f1aaa31d6fae8276b2cc85dbbefbdb8
Mykhailo Klapchuk <mklap@softserveinc.com>
2023-08-18 13:14:17 +0200

    UC#1 Create sort function by name

--- UC#1/Controllers/CountryController.cs
+++ UC#1/Controllers/CountryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Formats.Asn1;
 using System.Text.Json;
+using System.Xml.Linq;
 using UC_1.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,6 +12,17 @@ namespace UC_1.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
+        private const string URL = "https://restcountries.com/v3.1/all";
+        private static readonly List<Country> countriesData;
+
+        public CountryController()
+        {
+            if (countriesData == null)
+            {
+                GetCountriesData();
+            }
+        }
+
         // GET: api/<CountryController>
         [HttpGet]
         public async Task<List<Country>> GetCountries(
@@ -19,38 +31,62 @@ namespace UC_1.Controllers
             string nameSort = "not",
             int numberOfPages = 0)
         {
-            var countryList = new List<Country>();
+            return countriesData ?? new List<Country>();
+        }
 
+        public List<Country> GetCountriesFirteredByName(string nameFilter) 
+        {
+            if (string.IsNullOrEmpty(nameFilter))
+            {
+                return countriesData;
+            }
+            return countriesData.Where(x => x.Name.Common.Contains(nameFilter, StringComparison.InvariantCultureIgnoreCase)).ToList();
+        }
+
+        public List<Country> GetCountriesFirteredByPopulation(int popFilter)
+        {
+            if (popFilter < 1)
+            {
+                return countriesData;
+            }
+            return countriesData.Where(x => x.Population < (popFilter * 1000000)).ToList();
+        }
+
+        public List<Country> GetCountriesSortedByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                if (name.ToLower() == "ascend")
+                {
+                    return countriesData.OrderBy(x => x.Name.Common).ToList();
+                }
+                else if (name.ToLower() == "descend")
+                {
+                    return countriesData.OrderByDescending(x => x.Name.Common).ToList();
+                }
+            }
+
+            return countriesData;
+        }
+
+        private async void GetCountriesData()
+        {
             using (var client = new HttpClient())
             {
-                var url = "https://restcountries.com/v3.1/all";
-                var response = await client.GetAsync(url);
+                var response = await client.GetAsync(URL);
                 if (!response.IsSuccessStatusCode)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Error occures during HTTP request:{response.StatusCode}");
-
-                    return countryList;
                 }
 
                 var countryDataString = await response.Content.ReadAsStringAsync();
                 var countries = JsonSerializer.Deserialize<List<Country>>(countryDataString);
                 if (countries != null)
                 {
-                    countryList.AddRange(countries);
+                    countriesData.AddRange(countries);
                 }
             }
-
-            return countryList;
-        }
-
-        public List<Country> GetCountriesFirteredByName(List<Country> countries, string nameFilter) 
-        {
-            return countries.Where(x => x.Name.Common.Contains(nameFilter, StringComparison.InvariantCultureIgnoreCase)).ToList();
-        }
-        public List<Country> GetCountriesFirteredByPopulation(List<Country> countries, int popFilter)
-        {
-            return countries.Where(x => x.Population < (popFilter * 1000000)).ToList();
         }
     }
 }
